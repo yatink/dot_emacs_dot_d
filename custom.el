e45c617d21e16e8fba44875aa5018de582c79988
Yatin Kanetkar <yatin.kanetkar@gmail.com>
2018-06-06 15:09:00 -0400

    First

---
+++ custom.el
@@ -0,0 +1,34 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (misterioso)))
+ '(inhibit-startup-screen t)
+ '(package-selected-packages
+   (quote
+    (
+     docker-compose-mode
+     dockerfile-mode
+     magit
+     go-mode
+     markdown-toc
+     markdown-mode
+     jedi
+     groovy-mode
+     rainbow-delimiters
+     rust-mode
+     helm-projectile
+     projectile
+     erlang
+     helm-ag
+     helm
+     flycheck))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,22 @@
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;;Add MELPA-STABLE to the package repos
+(require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+
+
+;;Don't use tabs
+(setq-default indent-tabs-mode nil)
+
+;;Bind M-x to helm-M-x
+(global-set-key (kdb "M-x") 'helm-M-x)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+
+(provide 'init)
+;;; init.el ends here
